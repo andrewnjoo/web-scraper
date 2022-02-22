f4921686d6abf2546e92f0f8c07942c0d1035ddf
Andrew Njoo <adnjoo@gmail.com>
2022-02-21 16:57:58 -0800

    wwr scraper

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # python
+
+
+Technologies:
+
+* BeautifulSoup4
+  
+* LXML
+
+* Requests
+
+```
+pip install beautifulsoup4
+pip install lxml
+```
+
+<!-- advice https://www.youtube.com/watch?v=XVv6mJpFOb0 -->
---
+++ wwr.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get(
+    'https://weworkremotely.com/categories/remote-full-stack-programming-jobs').text
+# print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='feature')
+for job in jobs:
+    company = job.find('span', class_='company')
+    title = job.find('span', class_='title')
+    location = job.find('span', class_='region company')
+    if 'North America' in location.text or 'USA' in location.text:
+        print(company.text, title.text)
