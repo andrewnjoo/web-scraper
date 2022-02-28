847d6b0ef281fdc1cf098cb103acb0f2f9f7f061
Andrew Njoo <adnjoo@gmail.com>
2022-02-28 10:06:05 -0800

    edit koans.py

--- koans/koans.py
+++ koans/koans.py
@@ -36,11 +36,31 @@ def natural_keys(text):
 
 
 links.sort(key=natural_keys)
-print(links)
+# print(links)
 
-# Write to file
+# Write to links.csv
 
-with open("output.csv", "w", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for link in links:
-        writer.writerow([link])
+# with open("links.csv", "w", newline='') as csvfile:
+#     writer = csv.writer(csvfile)
+#     for link in links:
+#         writer.writerow([link])
+
+
+def addRow(num):
+    temp = []
+    html_text_1 = requests.get(links[num]).text
+    soup = BeautifulSoup(html_text_1, 'lxml')
+    para = soup.find_all("font")
+    for p in para[1:-1]:
+        x = p.text.replace('\n\n', ' ').replace(
+            '\n', ' ').replace('\xa0', '').replace("\\'", "")
+        temp.append(x)
+
+    with open("text.csv", "a", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in temp:
+            writer.writerow([row])
+
+
+for idx, row in enumerate(links):
+    addRow(idx)
