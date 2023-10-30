f82b7de3e2aa56ab286d2b84618832356f90337c
Andrew Njoo <adnjoo@gmail.com>
2023-10-30 09:23:29 -0700

    helpers

--- koans/koans.py
+++ koans/koans.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-import re
+import sys
+from helpers import atof, natural_keys
 
 # Get data from webpage
 
@@ -9,31 +10,12 @@ html_text = requests.get(
     'https://ashidakim.com/zenkoans/').text
 soup = BeautifulSoup(html_text, 'lxml')
 links = list(filter(lambda x: x['href'].startswith(
-    'http://www.ashidakim.com/zenkoans/'), soup.find_all('a')))
+    'https://ashidakim.com/zenkoans/'), soup.find_all('a')))
+
+# print(soup)
 
 for i, link in enumerate(links):
     links[i] = link['href']
-
-# 'Human Sorting' helper function
-
-
-def atof(text):
-    try:
-        retval = float(text)
-    except ValueError:
-        retval = text
-    return retval
-
-
-def natural_keys(text):
-    '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    float regex comes from https://stackoverflow.com/a/12643073/190597
-    '''
-    return [atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text)]
-
 
 links.sort(key=natural_keys)
 # print(links)
